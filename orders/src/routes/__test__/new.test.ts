c3b481921eda6977df6ebbf9ecfed17a5fcc8a1f
Aditya Tiwari <aditya.tiwari@nextsteps.ai>
2021-06-25 15:46:38 +0530

    add order event

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,62 @@
+import request from 'supertest';
+import { app } from '../../app';
+import mongoose from 'mongoose';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
+
+it('return error if ticket does not exits', async () => {
+	const ticketId = mongoose.Types.ObjectId();
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ ticketId })
+		.expect(404);
+});
+
+it('return error if ticket already reserved', async () => {
+	const ticket = Ticket.build({
+		title: 'Movie',
+		price: 20,
+	});
+	await ticket.save();
+	const order = Order.build({
+		ticket,
+		userId: 'adafdatest',
+		status: OrderStatus.AwaitingPayment,
+		expiresAt: new Date(),
+	});
+	await order.save();
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ ticketId: ticket.id })
+		.expect(400);
+});
+
+it('reserved a ticket', async () => {
+	const ticket = Ticket.build({
+		title: 'Movie',
+		price: 20,
+	});
+	await ticket.save();
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ ticketId: ticket.id })
+		.expect(201);
+});
+
+it('emits an order created event', async () => {
+	const ticket = Ticket.build({
+		title: 'Movie',
+		price: 20,
+	});
+	await ticket.save();
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ ticketId: ticket.id })
+		.expect(201);
+	expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@sgtickets/common';
+import {
+	NotFoundError,
+	requireAuth,
+	validateRequest,
+	BadRequestError,
+	OrderStatus,
+} from '@sgtickets/common';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 router.post(
 	'/api/orders',
 	// @ts-ignore
@@ -15,8 +26,44 @@ router.post(
 			.custom((input: string) => mongoose.Types.ObjectId.isValid(input))
 			.withMessage('TicketId must be provided'),
 	],
+	validateRequest,
 	async (req: Request, res: Response) => {
-		res.send({});
+		const { ticketId } = req.body;
+
+		const ticket = await Ticket.findById(ticketId);
+		if (!ticket) {
+			throw new NotFoundError();
+		}
+
+		const isReserved = await ticket.isReserved();
+		if (isReserved) {
+			throw new BadRequestError('Ticket is already reserved');
+		}
+
+		const expiration = new Date();
+		expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+		const order = Order.build({
+			userId: req.currentUser!.id,
+			expiresAt: expiration,
+			status: OrderStatus.Created,
+			ticket,
+		});
+
+		await order.save();
+		//@ts-ignore
+		new OrderCreatedPublisher(natsWrapper.client).publish({
+			id: order.id,
+			status: order.status,
+			ticket: {
+				id: ticket.id,
+				price: ticket.price,
+			},
+			expiresAt: order.expiresAt.toISOString(),
+			userId: order.userId,
+		});
+
+		res.status(201).send(order);
 	}
 );
 
